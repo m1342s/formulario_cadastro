1244ace09771b82701c0fc68289ac4960e86f70a
Matheus <matheusviersa995@hotmail.com>
2025-10-24 13:03:38 -0300

    Código atualizado

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-document.getElementById("cep").addEventListener("blur",(evento)=>{
-    const elemento = evento.target;
-    const valor = elemento.value;
+const form = document.getElementById("formCadastro")
+const campos = ["cep","rua","bairro","cidade","estado"]
 
-    if(!(valor.length === 8)){
-        return;
+window.addEventListener("load",()=>{
+    const dados = JSON.parse(localStorage.getItem("cadastroUsuario"))
+    if(dados){
+        campos.forEach(campo =>{
+            document.getElementById(campo).value || ""
+
+        })
     }
-
-    fetch(`viacep.com.br/ws/${valor}/json`)
-    .then(resposta =>resposta.json())
-    .then(dados =>{
-        console.log(dados)
-        if(!dados.erro){
-            document.getElementById("bairro").value=dados.bairro;
-            document.getElementById("rua").value=dados.rua;
-            document.getElementById("cidade").value=dados.cidade;
-            document.getElementById("estado").value=dados.uf;
-            const dadosUsuario = JSON.parse(localStorage.getItem("cadastro"))
-            localStorage.setItem("cadastro",JSON.stringify(dadosUsuario))
-        }else{
-            alert("CEP não encontrado ou inválido")
-        }
-    })
-    .catch(error =>console.error(error))
 })
+
+form.addEventListener("submit",(evento)=>{
+    evento.preventDefault()
+    const dados = {}
+    campos.forEach(campo=>{
+       dados[campo]=document.getElementById(campo).value
+    })
+    localStorage.getItem("cadastroUsuario",JSON.stringify(dados))
+    alert("Dados salvos com sucesso!")
+})
+
+document.getElementById("cep").addEventListener("blur",()=>{
+    let cep = document.getElementById("cep").value.replace(/\D/g,"")
+    if(cep.length === 8){
+        fetch(`viacep.com.br/ws/${cep}/json/`)
+        .then(resposta =>resposta.json())
+        .then(dados =>{
+            if(dados.erro){
+                alert("CEP não encontrado ou inválido!")
+                return
+            }
+            document.getElementById("rua").value=dados.logradouro
+            document.getElementById("bairro").value=dados.bairro
+            document.getElementById("cidade").value=dados.cidade
+            document.getElementById("estado").value=dados.uf
+        })
+        .catch(()=>alert("Erro ao buscar o CEP!"))
+    }
+});
